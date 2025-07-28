4497316a1c0533cc726822bc0e474ee746af6496
Phạm Bảo Việt <baoviet2905@gmail.com>
2025-07-28 20:38:39 +0700

    Add renderExpenses function to display expense list and total

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const filterDateInput = document.getElementById('filter-date');
 const filterBtn = document.getElementById('filter-btn');
 const resetFilterBtn = document.getElementById('reset-filter');
 
-let expenses = []; // Array store expenses
+let expenses = JSON.parse(localStorage.getItem('expense')) || []; // Array store expenses
 
 // Function saveExpenses to localStorage
 function saveExpenses() {
@@ -26,10 +26,33 @@ function addExpense(description, amount) {
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   const description = descriptionInput.value.trim();
-  const amount = parseFloat(amountInput, value.trim()); // Parse the amount as a float
+  const amount = parseFloat(amountInput.value.trim()); // Parse the amount as a float
   if (description && !isNaN(amount) && amount > 0) {
     addExpense(description, amount);
+    renderExpenses();
     descriptionInput.value = '';
-    amountInput.value('');
+    amountInput.value = '';
   }
 });
+
+// function display
+function renderExpenses() {
+  expenseList.innerHTML = ''; // Clear the current list
+  let total = 0; // Initialize total to 0
+
+  expenses.forEach((expense) => {
+    // Loop through each expense
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${expense.date}</td>
+      <td>${expense.description}</td>
+      <td>${expense.amount.toLocaleString()} VND</td>
+      <td><button class ="delete-btn">Delete</button></td>
+    `;
+    expenseList.appendChild(row); // Append the row to the table body
+    total += expense.amount;
+  });
+
+  totalElement.textContent = `Total: ${total.toLocaleString()} VND`; // Update the total display
+}
+renderExpenses();
